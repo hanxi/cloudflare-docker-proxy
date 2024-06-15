7a125423f09c37b22f4947cc7a9ad67cd21eb8c1
涵曦 <im.hanxi@gmail.com>
2024-06-15 18:36:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  "cloudflare-docker-proxy.hanxi-info.workers.dev": "https://registry-1.docker.io",
+  "${workername}.${username}.workers.dev/": "https://registry-1.docker.io",
 };
 
 function routeByHosts(host) {
